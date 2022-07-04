2640c08b4dc62891c803d677a51570bb03de6ea4
Ian <ssianfilipe@gmail.com>
2022-07-03 23:00:50 -0300

    declare variables

--- 05-arrays-and-collections/app.js
+++ 05-arrays-and-collections/app.js
@@ -1,4 +1,4 @@
-foodList = [
+var foodList = [
   ['Frutas: '],
   ['Laticínios: '],
   ['Congelados:'],
@@ -6,11 +6,11 @@ foodList = [
 ]
 
 function shopList() {
-  for (i = 0; i >= 0; i++) {
-    shopListResponse = prompt('Você deseja adicionar uma comida na sua lista de compras? Digite "1" para "sim" e "2" para "não".')
+  for (let i = 0; i >= 0; i++) {
+    let shopListResponse = prompt('Você deseja adicionar uma comida na sua lista de compras? Digite "1" para "sim" e "2" para "não".')
     if (shopListResponse == 1) {
-      foodListResponse = prompt('Qual comida você deseja inserir?')
-      foodListCategory = prompt('Em qual categoria essa comida se encaixa? Digite "1" para "Frutas", "2" para  "Laticínios", "3" para "Congelados" e "4" para "Doces".')
+      let foodListResponse = prompt('Qual comida você deseja inserir?')
+      let foodListCategory = prompt('Em qual categoria essa comida se encaixa? Digite "1" para "Frutas", "2" para  "Laticínios", "3" para "Congelados" e "4" para "Doces".')
       if (foodListCategory == 1) {
         foodList[0].push(foodListResponse)
       } else if (foodListCategory == 2) {
